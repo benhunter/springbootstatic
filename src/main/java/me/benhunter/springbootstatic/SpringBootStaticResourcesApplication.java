3f8543b96fbf54ab6eaddd3a2916cc22e4c45208
Ben Hunter <code@benhunter.me>
2021-12-23 12:32:36 -0700

    resources/static/index.html

---
+++ src/main/java/me/benhunter/springbootstatic/SpringBootStaticResourcesApplication.java
@@ -0,0 +1,13 @@
+package me.benhunter.springbootstatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStaticResourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStaticResourcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/benhunter/springbootstatic/SpringBootStaticResourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package me.benhunter.springbootstatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStaticResourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
